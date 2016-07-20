f641c6550133073854d1e127bb6249b1663891df
gatojebv01 <bermudezjosee@hotmail.com>
2016-07-19 23:52:03 -0400

    Changing the folder core to only CoreFramework

--- src/ultra/Composer/UltraCoreInstaller.php
+++ src/ultra/Composer/UltraCoreInstaller.php
@@ -21,7 +21,7 @@ class UltraCoreInstaller extends LibraryInstaller
             );
         }
 
-        return 'CoreFramwork/'.substr($package->getPrettyName(), 3);
+        return 'CoreFramwork";
     }
 
     /**
